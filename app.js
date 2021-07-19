c01b195958a82307247eb7fd4c7893aa5070def2
Grace <grazia.cassanod@gmail.com>
2021-07-19 11:31:09 +0200

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 mongoose.connect(
-  "mongodb+srv://akmal:@km@l123@cluster0.gqckr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "mongodb+srv://test_01:test_01@cluster0.gqckr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
